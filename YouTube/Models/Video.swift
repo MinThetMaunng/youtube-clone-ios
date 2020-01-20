50e73f8404fb6f120c70551408b79a62ccf2962a
MinThetMaung <minthetmaung96@gmail.com>
2020-01-20 15:14:25 +0630

    setup video model

---
+++ YouTube/Models/Video.swift
@@ -0,0 +1,15 @@
+//
+//  Video.swift
+//  YouTube
+//
+//  Created by Min Thet Maung on 20/01/2020.
+//  Copyright © 2020 Myanmy. All rights reserved.
+//
+
+import UIKit
+
+class Video: NSObject {
+    
+    var title: String?
+    var thumbnailImageName: String?
+}
