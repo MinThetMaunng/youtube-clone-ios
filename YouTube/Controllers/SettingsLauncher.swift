354640820b5996b63e065f8e4a35d93368885c68
MinThetMaung <minthetmaung96@gmail.com>
2020-01-22 23:08:09 +0630

    add collection view(with animations)  on top of the overlay layer

--- YouTube/Controllers/SettingsLauncher.swift
+++ YouTube/Controllers/SettingsLauncher.swift
@@ -12,6 +12,13 @@ class SettingsLauncher: NSObject {
     
     let  blackView = UIView()
     
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        return cv
+    }()
+    
     @objc func showSettings() {
         if let window = UIApplication.shared.keyWindow {
             
@@ -20,18 +27,33 @@ class SettingsLauncher: NSObject {
             blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
             
             window.addSubview(blackView)
+            window.addSubview(collectionView)
+            
+            let height: CGFloat = 200 + (UIApplication.shared.keyWindow?.safeAreaInsets.bottom ?? 0)
+            let y = window.frame.height - height
+            collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
+            
             blackView.frame = window.frame
             blackView.alpha = 0
             
-            UIView.animate(withDuration: 0.5) {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                 self.blackView.alpha = 1
-            }
+                self.collectionView.frame = CGRect(x: 0,y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            }, completion: nil)
+            
+//            UIView.animate(withDuration: 0.5) {
+//                self.blackView.alpha = 1
+//                self.collectionView.frame = CGRect(x: 0,y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+//            }
         }
     }
     
     @objc func handleDismiss() {
-        UIView.animate(withDuration: 0.5) {
+        UIView.animate(withDuration: 0.3) {
             self.blackView.alpha = 0
+            if let window = UIApplication.shared.keyWindow {
+                self.collectionView.frame = CGRect(x: 0,y: window.frame.height , width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            }
         }
     }
     
